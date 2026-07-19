f133333cbcba8ba91aafd332086c613efd014351
Sanskruti Chikate <sanskrutichikate6@gmail.com>
2026-07-19 13:18:21 +0530

    Configure Supabase database

--- backend/config/db.js
+++ backend/config/db.js
@@ -6,11 +6,11 @@ dotenv.config();
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  connectionString: process.env.DATABASE_URL,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 pool.connect()
